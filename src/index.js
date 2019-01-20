970377b24dc584a0b8f95534fb975ece9907aee8
Anya Efimova <gani4ka_n@ukr.net>
2019-01-20 16:25:07 +0200

    trial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import "./scss/styles.scss";
 import "@babel/polyfill";
-
+import userProgress from "./js/temper";
 import View from "./js/view";
 import Model from "./js/model";
 import Controller from "./js/controller";
 
 import "./loading-bar/loading-bar";
 
-
 const view = new View();
 const model = new Model();
+// userProgress(500);
 
 const controller = new Controller(model, view);
-

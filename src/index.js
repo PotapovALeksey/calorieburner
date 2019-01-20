711d50b29c03f85075d68c871b254485c0d2674a
Anya Efimova <gani4ka_n@ukr.net>
2019-01-20 17:41:47 +0200

    somthing

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import "@babel/polyfill";
 
 import userProgress from "./js/temper";
 
-import "./progress-bar/loading-bar"
-import "./progress-bar.js"
+import "./progress-bar/loading-bar";
+// import "./progress-bar.js"
 
 import View from "./js/view";
 import Model from "./js/model";
 import Controller from "./js/controller";
-import array from './js/array';
+import array from "./js/array";
 import "./loading-bar/loading-bar";
 
 import "./js/slider";
@@ -18,4 +18,3 @@ const view = new View();
 const model = new Model();
 
 const controller = new Controller(model, view);
-

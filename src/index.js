40d482d7c00e207cb4296d609ef94755bf464d6d
panterkocat <panterko@icloud.com>
2019-01-20 18:22:12 +0200

    update fix

--- src/index.js
+++ src/index.js
@@ -10,9 +10,5 @@ import "./js/slider";
 
 const view = new View();
 const model = new Model();
-<<<<<<< HEAD
 
 const controller = new Controller(model, view);
-=======
-const controller = new Controller(model, view);
->>>>>>> d068e05b82599926531bcd3c3f7903f9a3d40ed7

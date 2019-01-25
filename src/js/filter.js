88c706cbbe411c8cc4a75675b13e20e2c0c4f83a
Fedorchenko Anton <antony.corwin@gmail.com>
2019-01-25 23:43:44 +0200

    trash files

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,4 +1,144 @@
-import array
+// import * as obj from './array';
+
+
+const arrayVideos = [{
+    name: 'Утренняя тренировка для зарядки',
+    duration: '10 минут',
+    calories: '80 ккал',
+    inv: 'Коврик для йоги',
+    src: 'https://www.youtube.com/watch?v=ecrz1jaLV3A&list=PLUV7j0d8hiH2fO6jOTpwpKdvJA5fjnPaw',
+    level: 'Простая',
+    img: 'https://img.youtube.com/vi/ecrz1jaLV3A/sddefault.jpg',
+},
+    {
+        name: '5-ти минутная тренировка на пресс и ягодицы',
+        duration: '5 минут',
+        calories: '40 ккал',
+        inv: 'Коврик и мяч для йоги',
+        src: 'https://www.youtube.com/watch?v=iY4hQd24_d0',
+        level: 'Средняя',
+        img: 'https://img.youtube.com/vi/iY4hQd24_d0/sddefault.jpg',
+    },
+    {
+        name: 'Интенсивная тренировка на все группы мышц',
+        duration: '30 минут',
+        calories: '300 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=-9jok89UtQk',
+        level: 'Средняя',
+        img: 'https://img.youtube.com/vi/-9jok89UtQk/sddefault.jpg',
+    },
+    {
+        name: 'Йога с элементами суставной гимнастики',
+        duration: '50 минут',
+        calories: '350 ккал',
+        inv: 'Коврик для йоги',
+        src: 'https://www.youtube.com/watch?v=QDR_WB_2EHE',
+        level: 'Средняя',
+        img: 'https://img.youtube.com/vi/QDR_WB_2EHE/0.jpg',
+    },
+    {
+        name: 'Аэробика для дома для начинаючих',
+        duration: '50 минут',
+        calories: '400 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=d5bznQLUUuc',
+        level: 'Простая',
+        img: 'https://img.youtube.com/vi/d5bznQLUUuc/sddefault.jpg',
+    },
+    {
+        name: 'Функциональная фитнес тренировка',
+        duration: '60 минут',
+        calories: '600 ккал',
+        inv: 'Коврик для йоги',
+        src: 'https://www.youtube.com/watch?v=9tDnPfQWUbM',
+        level: 'Простая',
+        img: 'https://img.youtube.com/vi/9tDnPfQWUbM/sddefault.jpg',
+    },
+    {
+        name: 'Кардио тренировка с элементами бокса',
+        duration: '30 минут',
+        calories: '300 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=xSUXMwqZ3z0',
+        level: 'Средняя',
+        img: 'https://img.youtube.com/vi/xSUXMwqZ3z0/sddefault.jpg',
+    },
+    {
+        name: 'Интенсивная круговая тренировка',
+        duration: '45 минут',
+        calories: '500 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=yapXBQ9vBPQ',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/yapXBQ9vBPQ/sddefault.jpg',
+    },
+    {
+        name: '30-ти минутная тренировка с гантелями',
+        duration: '30 минут',
+        calories: '250 ккал',
+        inv: 'Гантели',
+        src: 'https://www.youtube.com/watch?v=Z_UWE3pfl6o',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/Z_UWE3pfl6o/sddefault.jpg',
+    },
+    {
+        name: 'Тренировка для рук с гантелями',
+        duration: '14 минут',
+        calories: '110 ккал',
+        inv: 'Коврик для йоги и гантели',
+        src: 'https://www.youtube.com/watch?v=oLOAffuSRds&t',
+        level: 'Простая',
+        img: 'https://img.youtube.com/vi/oLOAffuSRds/sddefault.jpg',
+    },
+    {
+        name: 'Короткая Табата тренировка',
+        duration: '10 минут',
+        calories: '100 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=irALl4gr9zA&t',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/irALl4gr9zA/sddefault.jpg',
+    },
+    {
+        name: '30-минутная хип-хоп тренировка по системе Табата',
+        duration: '30 минут',
+        calories: '350 ккал',
+        inv: 'Не нужен',
+        src: 'https://www.youtube.com/watch?v=kwkXyHjgoDM',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/kwkXyHjgoDM/sddefault.jpg',
+    },
+    {
+        name: 'Утренняя тренировка на 15 минут',
+        duration: '15 минут',
+        calories: '350 ккал',
+        inv: 'Коврик для йоги',
+        src: 'https://www.youtube.com/watch?v=1fDHrKRqy34',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/1fDHrKRqy34/sddefault.jpg',
+    },
+    {
+        name: '20-минутная жиросжигающая тренировка по системе Табата',
+        duration: '20 минут',
+        calories: '350 ккал',
+        inv: 'Коврик для йоги',
+        src: 'https://www.youtube.com/watch?v=lQkD1b5HOPY',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/lQkD1b5HOPY/sddefault.jpg',
+    },
+    {
+        name: '10 минутная тренировка для ягодиц средней сложности',
+        duration: '10 минут',
+        calories: '80 ккал',
+        inv: 'Коврик для йоги',
+        src: 'https://www.youtube.com/watch?v=RqfkrZA_ie0',
+        level: 'Сложная',
+        img: 'https://img.youtube.com/vi/RqfkrZA_ie0/sddefault.jpg',
+    }
+];
+
+// let videos = obj.arrayVideos;
 
 // get filters menu items
 const filterInventory = document.getElementById('js__filter-inventory');
@@ -46,201 +186,217 @@ const submenuDifficult = document.getElementById('js__submenu-difficult');
 const submenuCalory = document.getElementById('js__submenu-calory');
 const submenuTime = document.getElementById('js__submenu-time');
 
-// add event listeners
-document.addEventListener("DOMContentLoaded", addEvtListeners);
-
-function addEvtListeners() {
-  // add evtlisteners to menu items
-  filterInventory.addEventListener('click', showFilterInventory);
-  let submenuInventoryStyle = window.getComputedStyle(submenuInventory, "");
-  submenuInventory.style.display = (submenuInventoryStyle.display === 'none') ? 'none' : 'flex';
-  filterDifficult.addEventListener('click', showFilterDifficult);
-  let submenuDifficultStyle = window.getComputedStyle(submenuDifficult, "");
-  submenuDifficult.style.display = (submenuDifficultStyle.display === 'none') ? 'none' : 'flex';
-  filterCalory.addEventListener('click', showFilterCalory);
-  let submenuCaloryStyle = window.getComputedStyle(submenuCalory, "");
-  submenuCalory.style.display = (submenuCaloryStyle.display === 'none') ? 'none' : 'flex';
-  filterTime.addEventListener('click', showFilterTime);
-  let submenuTimeStyle = window.getComputedStyle(submenuTime, "");
-  submenuTime.style.display = (submenuTimeStyle.display === 'none') ? 'none' : 'flex';
-  // add evt listeners to menu Inventory subitems
-  inventoryNone.addEventListener('click', filterInventoryNone);
-  inventoryYogaCarpet.addEventListener('click', filterInventoryYogaCarpet);
-  inventoryDumbells.addEventListener('click', filterInventoryDumbells);
-  inventoryYogaCarpetAndBall.addEventListener('click', filterInventoryYogaCarpetAndBall);
-  inventoryYogaCarpetAndDumbells.addEventListener('click', filterInventoryYogaCarpetAndDumbells);
-  // add evt listeners to menu Difficult subitems
-  difficultEasy.addEventListener('click', filterDifficultNone);
-  difficultMedium.addEventListener('click', filterDifficultMedium);
-  difficultHard.addEventListener('click', filterDifficultHard);
-  // add evt listeners to menu Calory subitems
-  calory40.addEventListener('click', filterCalory40);
-  calory80.addEventListener('click', filterCalory80);
-  calory100.addEventListener('click', filterCalory100);
-  calory110.addEventListener('click', filterCalory110);
-  calory250.addEventListener('click', filterCalory250);
-  calory300.addEventListener('click', filterCalory300);
-  calory350.addEventListener('click', filterCalory350);
-  calory400.addEventListener('click', filterCalory400);
-  calory500.addEventListener('click', filterCalory500);
-  calory600.addEventListener('click', filterCalory600);
-  // add evt listeners to menu Time subitems
-  time5Min.addEventListener('click', filterTime5Min);
-  time10Min.addEventListener('click', filterTime10Min);
-  time14Min.addEventListener('click', filterTime14Min);
-  time20Min.addEventListener('click', filterTime20Min);
-  time30Min.addEventListener('click', filterTime30Min);
-  time45Min.addEventListener('click', filterTime45Min);
-  time50Min.addEventListener('click', filterTime50Min);
-  time60Min.addEventListener('click', filterTime60Min);
-}
-
+// show/close filter menus
 function showFilterInventory() {
-  if (submenuInventory.style.display === 'none') {
-    submenuInventory.style.display = 'flex';
-    submenuDifficult.style.display = 'none';
-    submenuCalory.style.display = 'none';
-    submenuTime.style.display = 'none';
-  } else {
-    submenuInventory.style.display = 'none';
-  }
+    if (submenuInventory.style.display === 'none') {
+        submenuInventory.style.display = 'flex';
+        submenuDifficult.style.display = 'none';
+        submenuCalory.style.display = 'none';
+        submenuTime.style.display = 'none';
+    } else {
+        submenuInventory.style.display = 'none';
+    }
 }
 
 function showFilterDifficult() {
-  if (submenuDifficult.style.display === 'none') {
-    submenuDifficult.style.display = 'flex';
-    submenuInventory.style.display = 'none';
-    submenuCalory.style.display = 'none';
-    submenuTime.style.display = 'none';
-  } else {
-    submenuDifficult.style.display = 'none';
-  }
+    if (submenuDifficult.style.display === 'none') {
+        submenuDifficult.style.display = 'flex';
+        submenuInventory.style.display = 'none';
+        submenuCalory.style.display = 'none';
+        submenuTime.style.display = 'none';
+    } else {
+        submenuDifficult.style.display = 'none';
+    }
 }
 
 function showFilterCalory() {
-  if (submenuCalory.style.display === 'none') {
-    submenuCalory.style.display = 'flex';
-    submenuInventory.style.display = 'none';
-    submenuDifficult.style.display = 'none';
-    submenuTime.style.display = 'none';
-  } else {
-    submenuCalory.style.display = 'none';
-  }
+    if (submenuCalory.style.display === 'none') {
+        submenuCalory.style.display = 'flex';
+        submenuInventory.style.display = 'none';
+        submenuDifficult.style.display = 'none';
+        submenuTime.style.display = 'none';
+    } else {
+        submenuCalory.style.display = 'none';
+    }
 }
 
 function showFilterTime() {
-  if (submenuTime.style.display === 'none') {
-    submenuTime.style.display = 'flex';
-    submenuInventory.style.display = 'none';
-    submenuDifficult.style.display = 'none';
-    submenuCalory.style.display = 'none';
-  } else {
-    submenuTime.style.display = 'none';
-  }
+    if (submenuTime.style.display === 'none') {
+        submenuTime.style.display = 'flex';
+        submenuInventory.style.display = 'none';
+        submenuDifficult.style.display = 'none';
+        submenuCalory.style.display = 'none';
+    } else {
+        submenuTime.style.display = 'none';
+    }
 }
 
-
-
-function filterInventoryNone() {
-  alert('It works!');
+// add event listeners
+function addEvtListeners() {
+    // add evtlisteners to menu items
+    filterInventory.addEventListener('click', showFilterInventory);
+    let submenuInventoryStyle = window.getComputedStyle(submenuInventory, "");
+    submenuInventory.style.display = (submenuInventoryStyle.display === 'none') ? 'none' : 'flex';
+    filterDifficult.addEventListener('click', showFilterDifficult);
+    let submenuDifficultStyle = window.getComputedStyle(submenuDifficult, "");
+    submenuDifficult.style.display = (submenuDifficultStyle.display === 'none') ? 'none' : 'flex';
+    filterCalory.addEventListener('click', showFilterCalory);
+    let submenuCaloryStyle = window.getComputedStyle(submenuCalory, "");
+    submenuCalory.style.display = (submenuCaloryStyle.display === 'none') ? 'none' : 'flex';
+    filterTime.addEventListener('click', showFilterTime);
+    let submenuTimeStyle = window.getComputedStyle(submenuTime, "");
+    submenuTime.style.display = (submenuTimeStyle.display === 'none') ? 'none' : 'flex';
 }
 
-function filterInventoryYogaCarpet() {
+document.addEventListener("DOMContentLoaded", addEvtListeners);
 
+// add evt listeners to menu Inventory subitems
+const filterBox = document.getElementById('filter_box');
+// const inputs = filterBox.querySelectorAll('input');
+// inputs.addEventListener('click', getChecked);
+
+filterBox.addEventListener('click', function (e) {
+    if (e.target.nodeName === 'INPUT') {
+        // console.log(e.target);
+        // getChecked(e)
+    }
+    console.log(e.target.nodeName);
+});
+console.log(filterBox);
+// filter functions
+const filter = {
+    inv: [],
+    level: [],
+    calories: [],
+    duration: [],
+};
+
+function getChecked(e) {
+    e.preventDefault();
+    const arrInputs = Array.from(inputs);
+    arrInputs.filter(input => input.checked).map(item => {
+        if (item.name === 'inventory') {
+            filter.inv.push(item.value)
+        }
+        if (item.name === 'difficult') {
+            filter.level.push(item.value)
+        }
+        if (item.name === 'calory') {
+            filter.calories.push(item.value)
+        }
+        if (item.name === 'time') {
+            filter.duration.push(item.value)
+        }
+    });
+
+    function filterItems(arrayVideos, filter) {
+        return undefined;
+    }
+
+    const finalArray = filterItems(arrayVideos, filter);
+    console.log(finalArray);
 }
 
-function filterInventoryDumbells() {
+const getVideoByInv = (arr, inventory) =>
+    arr.filter(product => inventory.length ?
+        inventory.some((inv) => product.inv === inv) :
+        true);
 
-}
+const getVideoByDifficult = (arr, difficulty) =>
+    arr.filter(product => difficulty.length ?
+        difficulty.some((difficult) => product.difficult === difficult) :
+        true);
 
-function filterInventoryYogaCarpetAndBall() {
+const getVideoByCalory = (arr, calories) =>
+    arr.filter(product => calories.length ?
+        calories.some((calory) => product.calory === calory) :
+        true);
 
-}
+const getVideoByTime = (arr, dates) =>
+    arr.filter(product => dates.length ?
+        dates.some((time) => product.time === time) :
+        true);
 
-function filterInventoryYogaCarpetAndDumbells() {
 
-}
 
-function filterDifficultNone() {
+// (function filterCatch() {
+//
+//     let filterInputs = document.querySelectorAll("input");
+//     let filterInventory = document.getElementsByClassName('check1');
+//     let filterDifficult = document.getElementsByClassName('check2');
+//     let filterCalory = document.getElementsByClassName('check3');
+//     let filterTime = document.getElementsByClassName('check4');
+//     if (filterInventory.checked || filterDifficult.checked || filterCalory.checked || filterTime.checked) {
+//         getChecked();
+//     }
+// }());
 
-}
 
-function filterDifficultMedium() {
+// //Первоначальная разметка
+// marcupHTML(laptops);
+// let filterBox = document.getElementById('filter_box');
+//по нажатию на кнопк 2 события: формирование объекта ФИЛЬТР и Выборка нужных карточек из Списка товаров
+// filterBox.addEventListener("click", getChecked);
+// filterBox.addEventListener("reset", clearFilter);
 
-}
 
-function filterDifficultHard() {
-
-}
-
-function filterCalory40() {
-
-}
-
-function filterCalory80() {
-
-}
-
-function filterCalory100() {
-
-}
-
-function filterCalory110() {
-
-}
-
-function filterCalory250() {
-
-}
-
-function filterCalory300() {
-
-}
-
-function filterCalory350() {
-
-}
-
-function filterCalory400() {
-
-}
-
-function filterCalory500() {
-
-}
-
-function filterCalory600() {
-
-}
-
-function filterTime5Min() {
-
-}
-
-function filterTime10Min() {
-
-}
-
-function filterTime14Min() {
-
-}
-
-function filterTime20Min() {
-
-}
-
-function filterTime30Min() {
-
-}
-
-function filterTime45Min() {
-
-}
-
-function filterTime50Min() {
-
-}
-
-function filterTime60Min() {
-
-}
+// const cards = document.querySelector(".js-cards");
+// const form = document.querySelector(".js-form");
+// const reset = form.querySelector('button[type="reset"]');
+//
+//
+// // const source = document.querySelector(".template-card").innerHTML.trim();
+//
+// // const template = Handlebars.compile(source);
+//
+// const filter = e => {
+//   e.preventDefault();
+//     debugger
+//   const filter = [...document.querySelectorAll("input:checked")].reduce(
+//     (acc, { name, value }) => {
+//       if (!acc.hasOwnProperty(name)) {
+//         acc[name] = [];
+//       }
+//
+//       acc[name].push(value);
+//
+//       return acc;
+//     },
+//     {}
+//   );
+//
+//   let isSize;
+//   let isColor;
+//   let isRelease_date;
+//
+//   const laptopsFilter = arrayVideos.filter(lap => {
+//
+//     if (filter.inventory) {
+//       isSize = filter.inventory.includes(String(lap.inventory));
+//     } else {
+//       isSize = true;
+//     }
+//     if (filter.difficult) {
+//       isColor = filter.difficult.includes(lap.difficult);
+//     } else {
+//       isColor = true;
+//     }
+//     if (filter.calory) {
+//       isRelease_date = filter.calory.includes(String(lap.calory));
+//     } else {
+//       isRelease_date = true;
+//     }
+//     console.log(isSize && isColor && isRelease_date);
+//     return isSize && isColor && isRelease_date;
+//   });
+//
+//   // CREATE HTML
+//   const markup = laptopsFilter.reduce((acc, lap) => (acc += template(lap)), "");
+//
+//   cards.innerHTML = markup;
+// };
+//
+// form.addEventListener("submit", filter);
+// reset.addEventListener("click", () => {
+//   cards.innerHTML = "";
+// });
